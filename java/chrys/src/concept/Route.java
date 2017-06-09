508ac3f4c51853aece775ed69a6975398a61d4e9
su <su@su.com>
2017-06-09 12:10:20 +0300

    .

---
+++ java/chrys/src/concept/Route.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package concept;
+
+/**
+ *
+ * @author su
+ */
+public class Route {
+    public Concept cpt = null;
+    
+    public Route(Concept cpt) {
+        this.cpt = cpt;
+    }
+}

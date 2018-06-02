71090579840969bade58732cd1678d665866d6e8
Konstantin Sudoplatov <konstantin.sudoplatov@yandex.ru>
2018-06-02 19:38:16 +0400

    .

--- java/chris/src/concepts/stat/LoadPremisesIntoFirstAssertion.java
+++ java/chris/src/concepts/stat/LoadPremisesIntoFirstAssertion.java
@@ -29,15 +29,15 @@ public class LoadPremisesIntoFirstAssertion extends StaticConcept {
         Assertion asrt = (Assertion)extra;     // the assertion object
         Neuron nr = (Neuron)nameSpace.get_cpt(paramCid[0]);     // the DynCptNameEnum.initial_concepts_for_start_of_console_chat concept
         
-        // unpack premises
-        for(Neuron.Premise prem: nr.get_premise()) {
-            asrt.add_premise(prem.cid);
-        }
-        
-        // unpack effects
-        for(long effCid: nr.get_effect_cid()) {
-            asrt.
-        }
+//        // unpack premises
+//        for(Neuron.Premise prem: nr.get_premise()) {
+//            asrt.add_premise(prem.cid);
+//        }
+//        
+//        // unpack effects
+//        for(long effCid: nr.get_effect_cid()) {
+//            asrt.
+//        }
         
         throw new UnsupportedOperationException("Not supported yet.");
     }

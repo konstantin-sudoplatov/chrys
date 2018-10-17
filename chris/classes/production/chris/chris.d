c9d4412068975d74b834362c6c595cc0df3c6ea6
Konstantin Sudoplatov <konstantin.sudoplatov@yandex.ru>
2018-10-17 19:43:09 +0400

    .

--- chris/classes/production/chris/chris.d
+++ chris/classes/production/chris/chris.d
@@ -1,13 +1,15 @@
 module chris;
 import std.stdio;
-import std.concurrency;
+import std.format, std.concurrency, core.thread;
 
-import project_params;
+import project_params, tools;
 
 import chri_shared;
 import stat.stat_registry;
 import cpt.cpt_stat;
 import atn.atn_dispatcher_thread;
+import console_thread;
+import messages;
 
 void main()
 {
@@ -33,5 +35,43 @@ void preloadConceptMaps(ref shared SpiritMap sm, ref immutable string[Cid] nm) {
     // Spawn the attention dispatcher thread.
     cast()_attnDispTid_ = spawn(&attention_dispatcher_thread_func);
 
+    // Spawn the console thread thread. We don't need to capture its tid. It will introduce itself to the attention circle
+    // thread and that would be enough.
     spawn(&console_thread_func);
+
+    // Wait for messages. That should be only requests for termination from console or exceptions that should be rethrown.
+    while(true) {
+        TerminateApp_msg termMsg;
+        Throwable ex;
+
+        Variant var;
+        receive(
+            (immutable TerminateApp_msg m){termMsg = cast()m;},
+            (shared Throwable e){ex = cast()e;},
+            (Variant v) {var = v;}
+        );
+
+        if      // TerminateAppMsg message has come?
+                (termMsg)
+        {   //yes: terminate other subthreads, terminate application
+            (cast()_attnDispTid_).send(new immutable TerminateApp_msg());
+            goto TERMINATE_APPLICATION;
+        }
+        else if // has one of the thead thrown an exception?
+                (ex)
+        {   // rethrow it
+            throw ex;
+        }
+        else if // has come an unexpected message?
+                (var.hasValue)
+        {   // log it
+            logit(format!"Unexpected message to the main thread: %s"(var.toString));
+        }
+    }
+
+TERMINATE_APPLICATION:
+    scope(exit) {
+        thread_joinAll;
+        writeln("good bye, world!"); stdout.flush;
+    }
 }
